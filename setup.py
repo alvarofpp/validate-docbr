b1221717c8cfb796b61491ba1045742d79d1b51a
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2019-03-08 10:27:30 -0300

    Gitignore, LICENSE, setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="validate-docbr",
+    version="0.0.1",
+    author="Álvaro Ferreira Pires de Paiva",
+    author_email="alvarofepipa@gmail.com",
+    description="Validate brazilian documents.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alvarofpp/validate-docbr",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
