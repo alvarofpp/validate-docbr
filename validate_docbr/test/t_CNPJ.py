df1c142aaa7a3dfc838f7851809f89d0998ff190
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2019-03-27 15:39:06 -0300

    Testes atendendo ao novo método

--- validate_docbr/test/t_CNPJ.py
+++ validate_docbr/test/t_CNPJ.py
@@ -2,7 +2,12 @@ from validate_docbr import CNPJ
 
 cnpj = CNPJ()
 
+# generate, generate(mask=True)
 cnpjs = [cnpj.generate() for i in range(10000)] + [cnpj.generate(mask=True) for i in range(10000)]
+# generate_list
+cnpjs = cnpjs + cnpj.generate_list(10000)\
+        + cnpj.generate_list(10000, True)\
+        + cnpj.generate_list(10000, True, True)
 
 for i in cnpjs:
     assert cnpj.validate(i)
--- validate_docbr/test/t_CPF.py
+++ validate_docbr/test/t_CPF.py
@@ -2,7 +2,12 @@ from validate_docbr import CPF
 
 cpf = CPF()
 
+# generate, generate(mask=True)
 cpfs = [cpf.generate() for i in range(10000)] + [cpf.generate(mask=True) for i in range(10000)]
+# generate_list
+cpfs = cpfs + cpf.generate_list(10000)\
+        + cpf.generate_list(10000, True)\
+        + cpf.generate_list(10000, True, True)
 
 for i in cpfs:
     assert cpf.validate(i)
