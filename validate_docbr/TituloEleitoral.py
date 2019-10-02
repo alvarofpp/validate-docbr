1aa5466963e78d30499afc14e7b19eafc8b2fa13
Luiz Pericolo <luizpericolo@gmail.com>
2019-10-01 22:49:48 -0300

    add class to create, validate and mask Titulo eleitoral
    * TODO: write tests

---
+++ validate_docbr/TituloEleitoral.py
@@ -0,0 +1,79 @@
+from .BaseDoc import BaseDoc
+
+from random import sample
+from typing import List
+
+
+class TituloEleitoral(BaseDoc):
+    def __init__(self):
+        self.digits = list(range(10))
+        self.first_check_digit_weights = list(range(2, 10))
+        self.second_check_digit_weights = list(range(7, 10))
+        self.first_check_digit_doc_slice = slice(0, 8)
+        self.second_check_digit_doc_slice = slice(8, 10)
+    
+    def compute_first_check_digit(self, doc_digits: List[int]) -> int:
+        doc_digits_to_consider = doc_digits[self.first_check_digit_doc_slice]
+        terms = [
+            doc_digit * multiplier
+            for doc_digit, multiplier in zip(doc_digits_to_consider, self.first_check_digit_weights)
+        ]
+
+        total = sum(terms)
+
+        if total % 11 == 10:
+            return 0
+
+        return total % 11
+
+    def compute_second_check_digit(self, doc_digits: List[int], first_check_digit: int) -> int:
+        doc_digits_to_consider = doc_digits[self.second_check_digit_doc_slice] + [first_check_digit]
+        terms = [
+            doc_digit * multiplier
+            for doc_digit, multiplier in zip(doc_digits_to_consider, self.second_check_digit_weights)
+        ]
+
+        total = sum(terms)
+
+        if total % 11 == 10:
+            return 0
+
+        return total % 11
+
+    def validate(self, doc: str = '') -> bool:
+        """Método para validar o documento desejado."""
+        doc_digits = list(map(int, self._only_digits(doc=doc)))
+
+        if len(doc_digits) != 12:
+            return False
+
+        first_check_digit = self.compute_first_check_digit(doc_digits=doc_digits)
+        second_check_digit = self.compute_second_check_digit(doc_digits=doc_digits, first_check_digit=first_check_digit)
+
+        return first_check_digit == doc_digits[-2] and second_check_digit == doc_digits[-1] 
+
+    def __generate_valid_state_identifier(self) -> str:
+        state_identifier = str(sample(range(1, 19), 1)[0])
+        return state_identifier.zfill(2)
+
+    def generate(self, mask: bool = False) -> str:
+        """Método para gerar um documento válido."""
+        document_digits = [sample(self.digits, 1)[0] for _ in range(8)]
+
+        state_identifier = self.__generate_valid_state_identifier()
+        document_digits.extend(map(int, state_identifier))
+
+        first_check_digit = self.compute_first_check_digit(doc_digits=document_digits)
+        second_check_digit = self.compute_second_check_digit(doc_digits=document_digits, first_check_digit=first_check_digit)
+
+        document_digits.extend([first_check_digit, second_check_digit])
+
+        document = ''.join(map(str, document_digits))
+        
+        if mask:
+            return self.mask(doc=document)
+        return document
+
+    def mask(self, doc: str = '') -> str:
+        """Mascara o documento enviado"""
+        return '{} {} {}'.format(doc[0:4], doc[4:8], doc[8:])
