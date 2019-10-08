4c1405a9df92dc60618554060c2857ca8af14ac9
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2019-10-08 11:20:11 -0300

    Arquivo test_all.py

---
+++ tests/test_all.py
@@ -0,0 +1,21 @@
+import unittest
+# Classes
+import tests.test_cpf
+import tests.test_cns
+import tests.test_cnpj
+
+
+def suite():
+    loader = unittest.TestLoader()
+    test_suite = unittest.TestSuite()
+
+    test_suite.addTests(loader.loadTestsFromModule(tests.test_cpf))
+    test_suite.addTests(loader.loadTestsFromModule(tests.test_cns))
+    test_suite.addTests(loader.loadTestsFromModule(tests.test_cnpj))
+
+    return suite
+
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
