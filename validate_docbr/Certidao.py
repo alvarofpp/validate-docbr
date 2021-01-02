54e6f2d8411753b66494071861ea6eecc1877b20
Ana Clara Cavalcante <anaclaracavalcante14@gmail.com>
2021-01-02 01:14:16 -0300

    Add generate to Certidao

--- validate_docbr/Certidao.py
+++ validate_docbr/Certidao.py
@@ -1,5 +1,5 @@
 from .BaseDoc import BaseDoc
-# from random import sample
+from random import sample
 
 
 class Certidao(BaseDoc):
@@ -10,7 +10,7 @@ class Certidao(BaseDoc):
 
     def validate(self, doc: str = '') -> bool:
         """Método para validar a Certidão de Nascimento/Casamento/Óbito."""
-        if not self._validate_input(doc, [' ']):
+        if not self._validate_input(doc, ['.', '-']):
             return False
 
         doc = self._only_digits(doc)
@@ -18,15 +18,12 @@ class Certidao(BaseDoc):
         if len(doc) != 32:
             return False
 
-        num = doc[:-2]
+        num = list(doc[:-2])
         dv = doc[-2:]
 
-        dv1 = self._weighted_sum(num) % 11
-        dv1 = 1 if dv1 > 9 else dv1
-        dv2 = self._weighted_sum(num+str(dv1)) % 11
-        dv2 = 1 if dv2 > 9 else dv2
+        expected_dv = self._generate_verifying_digit(num)
 
-        if dv == str(dv1) + str(dv2):
+        if dv == expected_dv:
             return True
 
         return False
@@ -45,10 +42,28 @@ class Certidao(BaseDoc):
 
     def generate(self, mask: bool = False) -> str:
         """Método para gerar a Certidão de Nascimento/Casamento/Óbito."""
-        pass
+        # Os trinta primeiros dígitos
+        certidao = [str(sample(self.digits, 1)[0]) for i in range(30)]
+
+        # Gerar os dígitos verificadores
+        certidao.append(self._generate_verifying_digit(certidao))
+
+        certidao = "".join(certidao)
+
+        return self.mask(certidao) if mask else certidao
 
     def mask(self, doc: str = '') -> str:
         """Mascara para a Certidão de Nascimento/Casamento/Óbito."""
         return "{}.{}.{}.{}.{}.{}.{}.{}-{}".format(
             doc[:6], doc[6:8], doc[8:10], doc[10:14],
-            doc[14], doc[15:20], doc[20:23], doc[23:29], doc[-2:])
+            doc[14], doc[15:20], doc[20:23], doc[23:30], doc[-2:])
+
+    def _generate_verifying_digit(self, doc: list) -> str:
+        """Gerar o dígito verificador da Certidao."""
+        dv1 = self._weighted_sum(doc) % 11
+        dv1 = 1 if dv1 > 9 else dv1
+
+        dv2 = self._weighted_sum(doc+[dv1]) % 11
+        dv2 = 1 if dv2 > 9 else dv2
+
+        return str(dv1)+str(dv2)
