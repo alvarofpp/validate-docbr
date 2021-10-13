523f3faf669977945f336b2dc98a727d986ebb5c
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2021-10-12 21:58:47 -0300

    feat: v1.9.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="validate_docbr",
-    version="1.8.2",
+    version="1.9.0",
     author="Álvaro Ferreira Pires de Paiva",
     author_email="alvarofepipa@gmail.com",
     description="Validate brazilian documents.",
