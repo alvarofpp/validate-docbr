55b809fe477b7e9b8f818a5809a46d9a046017f1
Eduardo Klosowski <eduardo.klosowski@contaswap.com>
2025-11-28 10:00:12 -0300

    feat: Informa que o pacote possui tipagem

--- setup.py
+++ setup.py
@@ -17,5 +17,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Typing :: Typed",
     ],
 )
